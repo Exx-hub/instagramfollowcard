fb5be63135dd86e0491861d5fa34138888df5c35
Exx-hub <61862900+Exx-hub@users.noreply.github.com>
2020-09-17 00:15:28 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import InstaFollow from "./InstaFollow";
+
+const App = () => {
+  return (
+    <div>
+      <InstaFollow />
+    </div>
+  );
+};
+
+export default App;
